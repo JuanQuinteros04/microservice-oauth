9cfdfcd08e55d695cd20f97ebf77145f91d12bb1
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2023-06-29 11:43:40 -0300

    First commit

---
+++ src/main/java/com/fromacionbdi/springboot/app/oauth/clients/UsuarioFeignClient.java
@@ -0,0 +1,13 @@
+package com.fromacionbdi.springboot.app.oauth.clients;
+
+import com.formacionbdi.springboot.app.commons.usuarios.model.entity.Usuario;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "servicio-usuarios")
+public interface UsuarioFeignClient {
+
+    @GetMapping("/usuarios/username/{username}")
+    Usuario findByUsername(@RequestParam String username);
+}

c9424f7e0297d6b04b58d9d4e4d6b2bb9bbd641b
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2023-07-11 19:05:17 -0300

    adding custom attributes in the trace

--- src/main/java/com/fromacionbdi/springboot/app/oauth/services/UsuarioService.java
+++ src/main/java/com/fromacionbdi/springboot/app/oauth/services/UsuarioService.java
@@ -1,7 +1,9 @@
 package com.fromacionbdi.springboot.app.oauth.services;
 
+import brave.Tracer;
 import com.formacionbdi.springboot.app.commons.usuarios.model.entity.Usuario;
 import com.fromacionbdi.springboot.app.oauth.clients.UsuarioFeignClient;
+import feign.FeignException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,29 +26,46 @@ public class UsuarioService implements IUsuarioService, UserDetailsService {
     @Autowired
     UsuarioFeignClient client;
 
+    @Autowired
+    private Tracer tracer;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        Usuario usuario = client.findByUsername(username);
+        try {
+            Usuario usuario = client.findByUsername(username);
 
-        if(usuario == null){
-            log.error("Error en el login, no existe el usuario '"+username+"' en el sistema");
-            throw new UsernameNotFoundException("Error en el login, no existe el usuario '"+username+"' en el sistema");
+            if (usuario == null) {
+                log.error("Error en el login, no existe el usuario '" + username + "' en el sistema");
+                throw new UsernameNotFoundException("Error en el login, no existe el usuario '" + username + "' en el sistema");
+            }
+
+            List<GrantedAuthority> authorities = usuario.getRoles()
+                    .stream()
+                    .map(role -> new SimpleGrantedAuthority(role.getNombre()))
+                    .peek(authority -> log.info("Role: " + authority.getAuthority()))
+                    .collect(Collectors.toList());
+            log.info("Usuario autenticado: " + username);
+
+            return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(), true, true, true,
+                    authorities);
+        }catch (FeignException e) {
+            String error = "Error en el login, no existe el usuario '" + username + "' en el sistema";
+            log.error("Error en el login, no existe el usuario '" + username + "' en el sistema");
+
+            tracer.currentSpan().tag("error.mensaje", error + ": " + e.getMessage());
+            throw new UsernameNotFoundException(
+                    "Error en el login, no existe el usuario '" + username+ "' en el sistema");
         }
-
-        List<GrantedAuthority> authorities = usuario.getRoles()
-                .stream()
-                .map(role -> new SimpleGrantedAuthority(role.getNombre()))
-                .peek(authority -> log.info("Role: " + authority.getAuthority()))
-                .collect(Collectors.toList());
-        log.info("Usuario autenticado: " + username);
-
-        return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(), true, true, true,
-        authorities);
     }
 
     @Override
     public Usuario findByUsername(String username) {
         return client.findByUsername(username);
+    }
+
+    @Override
+    public Usuario update(Usuario usuario, Long id) {
+        return client.update(usuario, id);
     }
 }
